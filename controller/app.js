55c0dc138b8e0686899ba73aca95b0a4a02761ce
Mohammad Mohammadi <imohammadd@gmail.com>
2017-03-25 10:43:09 +0430

    formating

--- controller/app.js
+++ controller/app.js
@@ -1,9 +1,10 @@
-var app = angular.module("myse", []);
+var app = angular.module("myse", ['ngSanitize']);
 
 app.controller('SearchCtrl', function($scope, $http) {
     $scope.items = [];
     $scope.msg = null;
-    $scope.search = function() {
+    $scope.search = function(start=11) {
+        console.log(start);
         $http({
             method: "get",
             url: "https://www.googleapis.com/customsearch/v1?key=AIzaSyD2n0m63W74jFZ0G6y_2JGBoRCB_xybEjw&cx=007033663672119120811:xyzdby8zhpa&q="+$scope.q
@@ -18,5 +19,5 @@ app.controller('SearchCtrl', function($scope, $http) {
         }, function(err) {
             console.error(err);
         });
-    }
+    };
 });
